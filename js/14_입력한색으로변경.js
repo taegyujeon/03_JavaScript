ffb5fc12b44f8544e22a96bce6c4a47fea018a81
Jtaegyu <tgjeon1104@naver.com>
2024-01-19 17:48:30 +0900

    20240119 수업내용 요소생성,함수,DOM

--- js/14_입력한색으로변경.js
+++ js/14_입력한색으로변경.js
@@ -1,3 +1,16 @@
-const box = document.querySelector("#box");
-const input = document.querySelector("#input");
+// 요소를 한 번에 여러개 얻어오면 배열 형태로 저장된다!
+// -> 얻어온 요소를 사용하려면 배열의 imdex별 요소를 하나씩 꺼내서 
+//  사용해야 한다!!
+
+// ex) boxList.style.color = "red";  // 배열의 글자색을 red 변경 (X)
+//    boxList[0].style.color = "blue"; // 0번 인덱스 요소 글자색 blue (O)
+
+const boxList = document.querySelector(".box");
+const inputList = document.querySelector(".color-input");
+
+document.querySelector("#changeButton").addEventListener("click",function(){
+    for(let i=0; i<boxList.length;i++){
+        boxList[i].style.background = inputList[i].value;
+    }
+});
 
---
+++ js/19_요소추가하기.js
@@ -0,0 +1,3 @@
+const input = document.createElement("input");
+    input.type="number";
+    console.log(input);
