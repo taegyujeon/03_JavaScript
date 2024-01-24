14420ba47a816b370fcfeddc482e545839acb54b
Jtaegyu <tgjeon1104@naver.com>
2024-01-24 17:18:39 +0900

    20240124 수업내용 시험공부

---
+++ js/29_클릭한숫자기록하기.js
@@ -0,0 +1,35 @@
+const numbers = document.querySelectorAll(".number");
+const result = document.querySelector("#result");
+
+for(let item of numbers){
+  item.addEventListener("click", e => {
+
+    if(result.textContent.length == 10){
+      alert("10글자 까지만 입력 가능");
+      return;
+    } 
+
+    result.textContent += e.target.textContent;
+  }); 
+}
+
+/* 초기화 버튼 */
+document.querySelector("#resetBtn").addEventListener("click", () => {
+  result.textContent = "";
+});
+
+
+// const numberBoard = document.querySelector("#numberBoard");
+// const resetBtn = document.querySelector("#resetBtn");
+// const clickNumber = document.querySelector("#clickNumber")
+
+// numberBoard.addEventListener("click",e=>{
+//     //console.log(e.target.textContent);
+//     if(clickNumber.textContent.length == 10) return;
+//     clickNumber.textContent += e.target.textContent
+
+// });
+
+// resetBtn.addEventListener("click",()=>{
+//     clickNumber.textContent = "";
+// });
